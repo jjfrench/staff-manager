5081ed0e2e12aca8bff09740a97d35d39b092a9c
jjfrench <jamisonjohnfrench@gmail.com>
2022-04-03 03:29:45 -0500

    Removed Commented Out Portion

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,11 +3,6 @@
         <v-row class="text-center">
             <v-col cols="12">
                 <CurrentWeek/>
-                <!-- <StandardSchedule
-                    v-for="schedule in subscriptions"
-                    :key="schedule.index"
-                    :data="schedule"
-                ></StandardSchedule> -->
             </v-col>
         </v-row>
     </v-container>
@@ -16,12 +11,10 @@
 <script>
 import { mapState } from 'vuex'
 import CurrentWeek from '../components/CurrentWeek';
-// import StandardSchedule from '../components/StandardSchedule';
 export default {
     name: 'dashboard',
     components: {
-        CurrentWeek,
-        // StandardSchedule
+        CurrentWeek
     },
     created() {
     },
