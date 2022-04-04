2402df35b0a5bfa5111397bc69318f56e68f27af
jjfrench <jamisonjohnfrench@gmail.com>
2022-04-03 19:40:48 -0500

    New Page Routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,26 @@ const routes = [
     path: '/',
     name: 'Dashboard',
     component: () => import('../views/Dashboard.vue')
+  },
+  {
+    path: '/Schedules',
+    name: 'Schedules',
+    component: () => import('../views/Schedules.vue')
+  },
+  {
+    path: '/Variances',
+    name: 'Variances',
+    component: () => import('../views/Variances.vue')
+  },
+  {
+    path: '/Homerooms',
+    name: 'Homerooms',
+    component: () => import('../views/Homerooms.vue')
+  },
+  {
+    path: '/Users',
+    name: 'Users',
+    component: () => import('../views/Users.vue')
   }
 ]
 
