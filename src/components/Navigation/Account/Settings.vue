00124199833a05c1142e80dc944bbd970140578d
jjfrench <jamisonjohnfrench@gmail.com>
2022-04-06 00:08:07 -0500

    Account Settings Component

---
+++ src/components/Navigation/Account/Settings.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-tooltip left>
+        <template v-slot:activator="{ }">
+            <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-width="200"
+            offset-x
+            >
+                <template v-slot:activator="{ on, attrs}">
+                    <v-btn
+                    icon
+                    small
+                    v-bind="attrs"
+                    v-on="on"
+                    >
+                        <v-icon>mdi-cog-outline</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-card>
+                    Hello
+                </v-card>
+            </v-menu>
+        </template>
+        <span>Settings</span>
+    </v-tooltip>
+</template>
+
+<script>
+export default {
+    name: 'Settings',
+	data: function() {
+		return {
+            fav: true,
+            menu: false,
+            message: false,
+            hints: true,
+		}
+	},
+    methods: {
+    }
+}
+</script>
+
+<style>
+
+</style>
